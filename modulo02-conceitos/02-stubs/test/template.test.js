02df74d07132edb9569aa1863a3084655b7afb58
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2024-01-31 14:13:00 -0300

    Add Service Test Suite with read method test

--- modulo02-conceitos/02-stubs/test/template.test.js
+++ modulo02-conceitos/02-stubs/test/template.test.js
@@ -1,9 +1,30 @@
-import { it, expect } from '@jest/globals'
+import { it, expect, describe, beforeEach, jest } from '@jest/globals'
+import Service from '../src/service'
+import fs from 'node:fs/promises'
 
-function sum(a, b) {
-  return a + b
-}
 
-it('sums two values', () => {
-    expect(sum(1, 2)).toBe(3) 
+describe('Service Test Suit', () => {
+  let _service
+
+  const filename = 'testfile.ndjson'
+
+  beforeEach(() => {
+    _service = new Service({
+        filename: filename
+    })
+  })
+
+  describe('#read', () => {
+    it('should return an empty array if the file is empty', async () => {
+      jest.spyOn(
+        fs,
+        fs.readFile.name
+      ).mockResolvedValue('')
+
+      const result = await _service.read()
+      console.log(result)
+
+      expect(result).toEqual([])
+    })
+  })
 })
