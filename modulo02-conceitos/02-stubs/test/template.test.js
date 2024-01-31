8f59557b1d20e04aa449ea62b8e20cb477d8d1fc
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2024-01-31 14:24:48 -0300

    Add test case to read users without password from file

--- modulo02-conceitos/02-stubs/test/template.test.js
+++ modulo02-conceitos/02-stubs/test/template.test.js
@@ -26,5 +26,36 @@ describe('Service Test Suit', () => {
 
       expect(result).toEqual([])
     })
+
+    it('should return users without password if file contains users', async () => {
+      // Arrange
+      const dbData = [
+        {
+          username: 'user1',
+          password: 'password',
+          createdAt: '2021-09-15T00:00:00.000Z',
+        },
+        {
+          username: 'user2',
+          password: 'password2',
+          createdAt: '2021-09-15T00:00:00.000Z',
+        },
+      ]
+
+      const fileContents = dbData
+        .map(item => JSON.stringify(item).concat('\n')).join('')
+
+      jest.spyOn(
+        fs,
+        'readFile'
+      ).mockResolvedValue(fileContents)
+
+      // Act
+      const result = await _service.read()
+
+      // Assert
+      const expectedResult = dbData.map(({password, ...rest}) => ({...rest}))
+      expect(result).toEqual(expectedResult)
+    })
   })
 })
